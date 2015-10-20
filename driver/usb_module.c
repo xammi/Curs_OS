a0b8a5617d781b9fcd920c17b46a2241179d91b7
Maksim Kislenko <kmg.xammi.1@gmail.com>
2015-10-21 00:17:10 +0300

    Initial android app

--- driver/usb_module.c
+++ driver/usb_module.c
@@ -5,10 +5,35 @@
 #include <linux/vmalloc.h>
 #include <asm/uaccess.h>
 #include <linux/seq_file.h>
+#include <linux/usb.h>
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("USB controller for mouse");
 MODULE_AUTHOR("Kislenko Maksim");
+
+#define MY_DEVICE_NAME    "my_usb_device"
+#define PRODUCT_ID 0x1
+#define VENDOR_ID 0x1234
+
+static struct usb_device_id my_table [] = {
+    { USB_DEVICE(VENDOR_ID, PRODUCT_ID) },
+    {}
+};
+
+static int my_probe(struct usb_interface *interface, const struct usb_device_id *id) {
+    // ...
+}
+
+static void my_disconnect(struct usb_interface *interface) {
+    // ...
+}
+
+static struct usb_driver my_driver = {
+  .name = MY_DEVICE_NAME,
+  .probe = my_probe,
+  .disconnect = my_disconnect,
+  .id_table = my_table,
+};
 
 static struct proc_dir_entry *proc_entry;
 
@@ -22,20 +47,24 @@ static const struct file_operations proc_file_fops = {
 
 int init_usb_module(void) {
     int ret = 0;
-    proc_entry = proc_create("usb_module", 0644, NULL, &proc_file_fops);
 
+    proc_entry = proc_create("usb_module", 0644, NULL, &proc_file_fops);
     if (proc_entry == NULL) {
-        ret = -ENOMEM;
         printk(KERN_INFO "usb_module: Couldn't create proc entry\n");
+        return -ENOMEM;
     }
-    else {
-        printk(KERN_INFO "usb_module: Module loaded.\n");
+
+    ret = usb_register(&my_driver);
+    if (ret != 0) {
+        printk(KERN_INFO "usb_module: Couldn't register usb driver\n");
+        return -ENOMEM;
     }
     return ret;
 }
 
 void cleanup_usb_module(void) {
     remove_proc_entry("usb_module", proc_entry);
+    usb_deregister(&my_driver);
     printk(KERN_INFO "usb_module: Module cleaned up.\n");
 }
 
--- phone/app/src/main/java/bmstu/curs_os/MainActivity.java
+++ phone/app/src/main/java/bmstu/curs_os/MainActivity.java
@@ -1,39 +1,168 @@
 package bmstu.curs_os;
 
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.usb.UsbConstants;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbEndpoint;
+import android.hardware.usb.UsbInterface;
+import android.hardware.usb.UsbManager;
+import android.hardware.usb.UsbRequest;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Iterator;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private TextView lgView;
+    private UsbManager mUsbManager;
+    private UsbDevice mDevice;
+    private UsbDeviceConnection mConnection;
+    private UsbEndpoint mEndpointIntr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lgView = (TextView)findViewById(R.id.log_view);
+        mUsbManager = (UsbManager)getSystemService(Context.USB_SERVICE);
     }
 
+    public void onResume() {
+        super.onResume();
+        //заполняем контейнер списком устройств
+        HashMap<String, UsbDevice> deviceList = mUsbManager.getDeviceList();
+        Iterator<UsbDevice> deviceIterator = deviceList.values().iterator();
+
+        lgView.setText( "Devices Count:" + deviceList.size() );
+
+        while (deviceIterator.hasNext()) {
+            UsbDevice device = (UsbDevice) deviceIterator.next();
+
+            //пример определения ProductID устройства
+            lgView.setText( lgView.getText() + "\n" + "Device ProductID: " + device.getProductId() );
+        }
+        //определяем намерение, описанное в фильтре
+        // намерений AndroidManifest.xml
+        Intent intent = getIntent();
+        lgView.setText( lgView.getText() + "\n" + "intent: " + intent);
+        String action = intent.getAction();
+
+        //если устройство подключено, передаем ссылку в
+        //в функцию setDevice()
+        UsbDevice device = (UsbDevice)intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+        if (UsbManager.ACTION_USB_DEVICE_ATTACHED.equals(action)) {
+            setDevice(device);
+            lgView.setText( lgView.getText() + "\n" + "UsbManager.ACTION_USB_DEVICE_ATTACHED.equals(action) is TRUE");
+            sendMessage("Hello world");
+        } else if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action)) {
+            if (mDevice != null && mDevice.equals(device)) {
+                setDevice(null);
+                lgView.setText( lgView.getText() + "\n" + "UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action) is TRUE");
+            }
+        }
+    }
+
+    private void setDevice(UsbDevice device) {
+        lgView.setText( lgView.getText() + "\n" + "setDevice " + device);
+        //определяем доступные интерфейсы устройства
+        if (device.getInterfaceCount() != 1) {
+
+            lgView.setText( lgView.getText() + "\n" + "could not find interface");
+            return;
+        }
+        UsbInterface intf = device.getInterface(0);
+
+        //определяем конечные точки устройства
+        if (intf.getEndpointCount() == 0) {
+
+            lgView.setText( lgView.getText() + "\n" +  "could not find endpoint");
+            return;
+        } else {
+            lgView.setText( lgView.getText() + "\n" + "Endpoints Count: " + intf.getEndpointCount() );
+        }
+
+        UsbEndpoint epIN = null;
+        UsbEndpoint epOUT = null;
+
+        //ищем конечные точки для передачи по прерываниям
+        for (int i = 0; i < intf.getEndpointCount(); i++) {
+            if (intf.getEndpoint(i).getType() == UsbConstants.USB_ENDPOINT_XFER_INT) {
+                if (intf.getEndpoint(i).getDirection() == UsbConstants.USB_DIR_IN) {
+                    epIN = intf.getEndpoint(i);
+                    lgView.setText( lgView.getText() + "\n" + "IN endpoint: " + intf.getEndpoint(i) );
+                }
+                else {
+                    epOUT = intf.getEndpoint(i);
+                    lgView.setText( lgView.getText() + "\n" + "OUT endpoint: " + intf.getEndpoint(i) );
+                }
+            } else { lgView.setText( lgView.getText() + "\n" + "no endpoints for INTERRUPT_TRANSFER"); }
+        }
+
+        mDevice = device;
+        mEndpointIntr = epOUT;
+
+        //открываем устройство для передачи данных
+        if (device != null) {
+            UsbDeviceConnection connection = mUsbManager.openDevice(device);
+            if (connection != null && connection.claimInterface(intf, true)) {
+
+                lgView.setText( lgView.getText() + "\n" + "open device SUCCESS!");
+                mConnection = connection;
+
+            } else {
+
+                lgView.setText( lgView.getText() + "\n" + "open device FAIL!");
+                mConnection = null;
+            }
+        }
+    }
+
+    void sendMessage(String message) {
+        //определение размера буфера для отправки
+        //исходя из максимального размера пакета
+        int bufferDataLength = mEndpointIntr.getMaxPacketSize();
+        lgView.setText( lgView.getText() + "\n" + mEndpointIntr.getMaxPacketSize() );
+
+        ByteBuffer buffer = ByteBuffer.allocate(bufferDataLength + 1);
+        UsbRequest request = new UsbRequest();
+        buffer.put(message.getBytes());
+        request.initialize(mConnection, mEndpointIntr);
+        request.queue(buffer, bufferDataLength);
+
+        try {
+            if (request.equals(mConnection.requestWait())) {
+                //отправка прошла успешно
+                lgView.setText( lgView.getText() + "\n" +  "sending CLEAR!!!");
+            }
+        }
+        catch (Exception ex) {
+            //что-то не так...
+            lgView.setText( lgView.getText() + "\n" +  "sending not clear...");
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
